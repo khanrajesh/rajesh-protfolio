83ccf316974436d87c449c376e227bfc500eb2e9
Rajesh khan <rajesh.k@iserveu.co.in>
2023-07-01 02:48:30 +0530

    added timeline section and animation in it

--- src/components/TimeLine.js
+++ src/components/TimeLine.js
@@ -1,9 +1,66 @@
+import {Col, Container, Row} from "react-bootstrap";
+import {CardinalCurve} from "react-svg-curve";
+import {useState, useEffect} from "react";
+import p1 from '../assets/img/p1.png'
+import p2 from '../assets/img/p2.png'
+import p3 from '../assets/img/p3.png'
+import p4 from '../assets/img/p4.png'
+import p5 from '../assets/img/p5.png'
+import p6 from '../assets/img/p6.png'
+import p7 from '../assets/img/p7.png'
+import p8 from '../assets/img/p8.png'
+import p9 from '../assets/img/p9.png'
+import p10 from '../assets/img/p10.png'
+import p11 from '../assets/img/p11.png'
+import {Box} from "react-bootstrap-icons";
 
+export const TimeLine = () => {
+    const data = [
 
-export const TimeLine = () =>{
+        [1250, 80],
+        [400, 270],
+        [1000, 570],
+        [10, 950],
+    ];
+
     return (
         <section className="time-line" id="time-line">
+            <Container>
+                <div>
+                    <svg className="curve">
+                        <CardinalCurve className="cardinal-curve" data={data} tension={0}/>
+                    </svg>
 
+                    <img className="p-image" id="p1" src={p1} alt="p1"/>
+                    <img className="p-image" id="p2" src={p2} alt="p2"/>
+                    <img className="p-image" id="p12" src={p2} alt="p12"/>
+                    <img className="p-image" id="p3" src={p3} alt="p3"/>
+                    <img className="p-image" id="p4" src={p4} alt="p4"/>
+                    <img className="p-image" id="p5" src={p5} alt="p5"/> {/*red planet*/}
+                    <img className="p-image" id="p6" src={p6} alt="p6"/>
+                    <img className="p-image" id="p7" src={p7} alt="p7"/>
+                    {/*<img className="p-image" id="p10" src={p10} alt="p10"/>*/}
+                    <img className="p-image" id="p11" src={p11} alt="p11"/> {/*last blue planet*/}
+
+                    <TimeLineCard title="title" description="dess"/>
+                    <TimeLineCard title="tlee" description="destts" date="2022 - 2023"/>
+
+
+                </div>
+
+            </Container>
         </section>
+    );
+};
+
+
+export const TimeLineCard = ({title, date, description}) => {
+    return (
+        <Col className="time-line-card" size={12} sm={6} md={4}>
+            <div className="">
+                <h6>{title} <span>({date})</span></h6>
+                <p>{description}</p>
+            </div>
+        </Col>
     )
 }
